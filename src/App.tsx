a428bcea9202ef995837b7e2997988077f82a6e3
Florian <floriandurand09@gmail.com>
2024-06-06 19:40:44 +0200

    add rotating arm structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,57 @@
 import * as THREE from 'three'
-import { useRef, useState } from 'react'
-import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
+import { useRef, useState, useEffect, forwardRef } from 'react'
+import { Canvas, useFrame, ThreeElements, useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-function Box(props: ThreeElements['mesh']) {
-  const meshRef = useRef<THREE.Mesh>(null!)
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
 
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta))
-
-  return (
-    <mesh
-      {...props}
-      ref={meshRef}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
+function CenterCylinder(props: ThreeElements['mesh']) {
+  const scene = useLoader(GLTFLoader, "CenterCylinder.glb").scene;
+  return <primitive {...props} object={scene} />
 }
+
+function Arm(props: ThreeElements['mesh']) {
+  const scene = useLoader(GLTFLoader, "Arm.glb").scene;
+
+  return <primitive {...props} object={scene} />
+}
+
+const Plop = forwardRef(function Stand(props: ThreeElements['mesh'], ref) {
+  const scene = useLoader(GLTFLoader, "Stand.glb").scene;
+
+  return <primitive ref={ref} {...props} object={scene} />
+})
+
+function ArmStuct() {
+  const ref = useRef<THREE.Group>(null!)
+  const standRef = useRef<typeof Plop>(null!)
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta
+    var oldPosition = new THREE.Vector3(0, -1.5, 10)
+    oldPosition.applyAxisAngle(new THREE.Vector3(1, 0, 0), ref.current.rotation.x)
+    console.log(oldPosition)
+    standRef.current.position.x = oldPosition.x
+    standRef.current.position.y = oldPosition.y
+    standRef.current.position.z = oldPosition.z
+  })
+
+  return <group><group ref={ref} position={[0, 0, 0]}>
+    <CenterCylinder position={[0, 0, 0]} />
+    <Arm position={[0, 0, 0]} />
+  </group><Plop ref={standRef} position={[0, 0, 0]}></Plop></group>
+
+}
+
 
 function App() {
 
   return (
-    <Canvas>
+    <Canvas camera={{ fov: 75, position: [0, 10, -20] }}>
       <ambientLight intensity={Math.PI / 2} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </Canvas>
+      <ArmStuct />
+    </Canvas >
   )
 }
 
