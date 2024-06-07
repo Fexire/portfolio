8818722d3789a6a7c9dcc8ccfd9282b544d56e1e
Florian <floriandurand09@gmail.com>
2024-06-07 17:35:31 +0200

    add procedural stands with capsules

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,94 @@
 import * as THREE from 'three'
-import { useRef, useState, useEffect, forwardRef, useMemo } from 'react'
-import { Canvas, useFrame, ThreeElements, useLoader, ThreeEvent } from '@react-three/fiber'
+import { useRef, useState, useMemo } from 'react'
+import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
 import { useGLTF } from '@react-three/drei'
 
 
 function CenterCylinder(props: ThreeElements['mesh']) {
   const scene = useGLTF("CenterCylinder.glb").scene;
-  return <primitive {...props} object={scene} />
+  const clone = useMemo(() => scene.clone(), [scene]);
+  return <primitive {...props} object={clone} />
 }
 
 function Arm(props: ThreeElements['mesh']) {
   const scene = useGLTF("Arm.glb").scene;
-
-  return <primitive {...props} object={scene.clone()} />
+  const clone = useMemo(() => scene.clone(), [scene]);
+  return <primitive {...props} object={clone} />
 }
 
-const Plop = forwardRef(function Stand(props: ThreeElements['mesh'], ref) {
+function Stand(props: ThreeElements['mesh']) {
   const scene = useGLTF("Stand.glb").scene;
+  const clone = useMemo(() => scene.clone(), [scene]);
+  return <primitive {...props} object={clone} />
+}
 
-  return <primitive ref={ref} {...props} object={scene} />
-})
+function Capsule(props: ThreeElements['mesh']) {
+  const scene = useGLTF("Capsule.glb").scene;
+  const clone = useMemo(() => scene.clone(), [scene]);
+  return <primitive {...props} object={clone} />
+}
 
+interface StandWithCapsulesProps {
 
-interface ArmStructPropos {
+  capsuleNumber: number
+  position: THREE.Vector3
+  key: number
+}
+
+function StandWithCapsules(props: StandWithCapsulesProps) {
+
+  var [position, setPosition] = useState(new THREE.Vector3(0, 0, 0))
+
+  var [capsulePositions, setCapsulePositions] = useState(Array<THREE.Vector3>(props.capsuleNumber).fill(new THREE.Vector3(0, 0, 0)).map((_, i) => {
+    const gap = 19 / (props.capsuleNumber - 1);
+    const start = -10;
+    return new THREE.Vector3(start + i * gap, 1, 0);
+  }))
+
+  useFrame((state, delta) => {
+    setPosition(new THREE.Vector3(props.position.x, props.position.y, props.position.z))
+  })
+
+  function generateCapsules() {
+    return Array(props.capsuleNumber).fill(0).map((_, i) => {
+      return <Capsule key={i} position={capsulePositions[i]} />
+    })
+  }
+
+  return <group position={position}>
+    <Stand />
+    {generateCapsules()}
+  </group>
+
+}
+
+interface ArmStructProps {
   scrollDeltaY: number
   armsNumber: number
 }
 
-function ArmStuct(props: ArmStructPropos) {
+function ArmStuct(props: ArmStructProps) {
   const ref = useRef<THREE.Group>(null!)
-  const standRef = useRef<THREE.Mesh>(null!)
   var [deltaY, setDeltaY] = useState(0)
   var [velocity, setVelocity] = useState(0)
   const stopForce = 0.001
   const maxVelocity = 0.1
+  var [standsPosition, setStandsPosition] = useState(Array<THREE.Vector3>(props.armsNumber).fill(new THREE.Vector3(0, 0, 0)).map((_, i) => {
+    var position = new THREE.Vector3(0, -1.5, 10);
+    position.applyAxisAngle(new THREE.Vector3(1, 0, 0), i * (2 * Math.PI / props.armsNumber))
+    return position;
+  }))
 
+  function moveStands() {
+    setStandsPosition((prevStandsPositions) => {
+      prevStandsPositions.forEach((e, i) => {
+        var position = new THREE.Vector3(0, -1.5, 10);
+        position.applyAxisAngle(new THREE.Vector3(1, 0, 0), ref.current.rotation.x + i * (2 * Math.PI / props.armsNumber))
+        e.copy(position)
+      })
+      return prevStandsPositions
+    })
+  }
 
   useFrame((state, delta) => {
     var currentVelocity = velocity
@@ -44,23 +98,33 @@ function ArmStuct(props: ArmStructPropos) {
     var velocitySign = Math.sign(currentVelocity)
     currentVelocity -= velocitySign * stopForce
     Math.sign(currentVelocity) != velocitySign ? currentVelocity = 0 : currentVelocity = currentVelocity
-
-    var oldPosition = new THREE.Vector3(0, -1.5, 10)
-    oldPosition.applyAxisAngle(new THREE.Vector3(1, 0, 0), ref.current.rotation.x)
-    standRef.current.position.x = oldPosition.x
-    standRef.current.position.y = oldPosition.y
-    standRef.current.position.z = oldPosition.z
-
+    moveStands()
     setVelocity(currentVelocity)
     setDeltaY(props.scrollDeltaY)
   })
 
-  return <group><group ref={ref} position={[0, 0, 0]}>
-    <CenterCylinder position={[0, 0, 0]} />
-    {Array(props.armsNumber).fill(0).map((_, i) => { var rotation_x = i * (2 * Math.PI / props.armsNumber); var position = new THREE.Vector3(0, 0, 3); position.applyAxisAngle(new THREE.Vector3(1, 0, 0), rotation_x); console.log(position); return <Arm key={i} position={position} rotation={[rotation_x, 0, 0]} />; })}
+  function generateArms() {
+    return Array(props.armsNumber).fill(0).map((_, i) => {
+      var rotation_x = i * (2 * Math.PI / props.armsNumber);
+      var position = new THREE.Vector3(0, 0, 3);
+      position.applyAxisAngle(new THREE.Vector3(1, 0, 0), rotation_x);
+      return <Arm key={i} position={position} rotation={[rotation_x, 0, 0]} />;
+    })
+  }
 
+  function generateStands() {
+    return Array(props.armsNumber).fill(0).map((_, i) => {
+      return <StandWithCapsules key={i} position={standsPosition[i]} capsuleNumber={4} />;
+    })
+  }
 
-  </group><Plop ref={standRef} position={[0, 0, 0]}></Plop></group>
+  return <group>
+    <group ref={ref} position={[0, 0, 0]}>
+      <CenterCylinder position={[0, 0, 0]} />
+      {generateArms()}
+    </group>
+    {generateStands()}
+  </group>
 
 }
 
@@ -70,11 +134,11 @@ function App() {
   const [scrollDeltaY, setScroll] = useState(0)
 
   return (
-    <Canvas onWheel={(e) => setScroll(scrollDeltaY + e.deltaY)} camera={{ fov: 75, position: [0, 10, -20] }}>
+    <Canvas onWheel={(e) => setScroll(scrollDeltaY + e.deltaY)} camera={{ fov: 75, position: [10, 10, -20] }}>
       <ambientLight intensity={Math.PI / 2} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <ArmStuct scrollDeltaY={scrollDeltaY} armsNumber={7} />
+      <ArmStuct scrollDeltaY={scrollDeltaY} armsNumber={8} />
     </Canvas >
   )
 }
